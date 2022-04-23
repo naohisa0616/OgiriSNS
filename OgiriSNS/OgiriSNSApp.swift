f2c03337022c51abf54ee4917839e8c4c9088d66
naohisa0616 <naohisa0616@icloud.com>
2022-04-23 21:09:36 +0900

    Initial Commit

---
+++ OgiriSNS/OgiriSNSApp.swift
@@ -0,0 +1,17 @@
+//
+//  OgiriSNSApp.swift
+//  OgiriSNS
+//
+//  Created by 宮崎直久 on 2022/04/23.
+//
+
+import SwiftUI
+
+@main
+struct OgiriSNSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
